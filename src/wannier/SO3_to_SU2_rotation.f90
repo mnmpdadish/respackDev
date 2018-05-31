53f604c6b4c3228abc7cb40aacb7700ae74746fc
Maxime Charlebois <mnmpdadish@hotmail.com>
2018-05-31 04:18:33 -0400

    testing an alternative for SO3_to_SU2_rotation.f90, inspired largely by quantum espresso divide_class_so.f90

--- src/wannier/SO3_to_SU2_rotation.f90
+++ src/wannier/SO3_to_SU2_rotation.f90
@@ -21,7 +21,7 @@ subroutine SO3_to_SU2_rotation(SO3_matrix, SU2_matrix)
   real(8)::angles(32),theta_2
   real(8)::tol=1e-8
   complex(8)::ci=(0.0d0,1.0d0)
-  
+
   !from quantum espresso:
   data s0/ 1.d0,  0.d0,  0.d0,  0.d0,  1.d0,  0.d0,  0.d0,  0.d0,  1.d0, &
           -1.d0,  0.d0,  0.d0,  0.d0, -1.d0,  0.d0,  0.d0,  0.d0,  1.d0, &
@@ -100,8 +100,15 @@ subroutine SO3_to_SU2_rotation(SO3_matrix, SU2_matrix)
            cos3,  sin3,  0.d0 /
 
 403 format(3(1x,F10.5,"        ",1x))
-404 format(2(1x,F20.5,SP,F20.5,"i        ",1x))
+404 format(2(1x,F20.2,SP,F20.2,"i   ",1x))
 
+  
+  !kk = 8
+  !call SO3_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),angles(kk), SO3_matrix)
+  !call from_SO3_matrix_to_SU2_matrix(SO3_matrix,SU2_matrix)
+  !stop
+  
+  
   
   !do kk=1,32
   !  call SU2_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),angles(kk)/2.0d0, SU2_matrix)
@@ -110,51 +117,73 @@ subroutine SO3_to_SU2_rotation(SO3_matrix, SU2_matrix)
   !  write(6,*)
   !  write(6,*)
   !  write(6,404) ((SU2_matrix(ii,jj), jj = 1,2), ii = 1,2)
-  !  
-  !  call SU2_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),angles(kk)/(-2.0d0), SU2_matrix)
+
+    
+  !  call from_SO3_matrix_to_SU2_matrix(SO3_matrix,SU2_matrix)
+    !call SU2_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),angles(kk)/(-2.0d0), SU2_matrix)
   !  write(6,*)
   !  write(6,404) ((SU2_matrix(ii,jj), jj = 1,2), ii = 1,2)
+    
+  !  call from_SO3_matrix_to_SU2_matrix(-SO3_matrix,SU2_matrix)
+    !call SU2_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),angles(kk)/(-2.0d0), SU2_matrix)
   !  write(6,*)
-  !  write(6,403) ((SO3_matrix(ii,jj), jj = 1,3), ii = 1,3)
-  !  write(6,*)
-  !  write(6,403) ((s0(ii,jj,kk), jj = 1,3), ii = 1,3)
+  !  write(6,404) ((SU2_matrix(ii,jj), jj = 1,2), ii = 1,2)
+    
+    !write(6,*)
+    !write(6,403) ((SO3_matrix(ii,jj), jj = 1,3), ii = 1,3)
+    !write(6,*)
+    !write(6,403) ((s0(ii,jj,kk), jj = 1,3), ii = 1,3)
   !enddo
   !stop
   
-  do kk=1,32
-    !write(6,*) kk
-    found=.true.
-    inv_found=.true.
-    do ii=1,3
-      do jj=1,3
-        !write(6,*) SO3_matrix(ii,jj), s0(ii,jj,kk)
-        if(abs(SO3_matrix(ii,jj)-s0(ii,jj,kk))>tol) found=.false.
-        if(abs(SO3_matrix(ii,jj)+s0(ii,jj,kk))>tol) inv_found=.false.
-      enddo
-    enddo
-    if((found==.true.).or.(inv_found==.true.)) then
-      exit
-      !write(6,*) 'symmetry found.'
-    elseif(kk==32) then
-      write(6,*) 'ERROR. rotation symmetry not found.'
-      stop 
-    endif
-  enddo
+  !do kk=1,32
+  !  !write(6,*) kk
+  !  found=.true.
+  !  inv_found=.true.
+  !  do ii=1,3
+  !    do jj=1,3
+  !      !write(6,*) SO3_matrix(ii,jj), s0(ii,jj,kk)
+  !      if(abs(SO3_matrix(ii,jj)-s0(ii,jj,kk))>tol) found=.false.
+  !      if(abs(SO3_matrix(ii,jj)+s0(ii,jj,kk))>tol) inv_found=.false.
+  !    enddo
+  !  enddo
+  !  if((found==.true.).or.(inv_found==.true.)) then
+  !    exit
+  !    !write(6,*) 'symmetry found.'
+  !  elseif(kk==32) then
+  !    write(6,*) 'ERROR. rotation symmetry not found.'
+  !    stop 
+  !  endif
+  !enddo
   
-  if(found==.true.) then
-    call SU2_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),angles(kk)/2.0d0, SU2_matrix)
-  elseif(inv_found==.true.) then
-    call SU2_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),-angles(kk)/2.0d0, SU2_matrix)
-  else
-    write(6,*) 'ERROR. rotation symmetry not found.'
-    stop 
-  endif
+  
+  
+  
+  
+  !if(found==.true.) then
+  !  call SU2_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),angles(kk)/2.0d0, SU2_matrix)
+  !elseif(inv_found==.true.) then
+  !  call SU2_rotation(n_vector(1,kk),n_vector(2,kk),n_vector(3,kk),-angles(kk)/2.0d0, SU2_matrix)
+  !else
+  !  write(6,*) 'ERROR. rotation symmetry not found.'
+  !  stop 
+  !endif
+  ! 
+  !write(6,*)
+  !write(6,404) ((SU2_matrix(ii,jj), jj = 1,2), ii = 1,2)
+  
+  
+  call from_SO3_matrix_to_SU2_matrix(SO3_matrix,SU2_matrix)
+  !write(6,*)
+  !write(6,404) ((SU2_matrix(ii,jj), jj = 1,2), ii = 1,2)
+  
   !do ii=1,3
   !  do jj=1,3
   !    if(abs(SO3_matrix(ii,jj)-s0(ii,jj,kk))<tol) found=.false.
   !    if(abs(SO3_matrix(ii,jj)+s0(ii,jj,kk))<tol) inv_found=.false.
   !  enddo
   !enddo
+  
   
   write(6,*)
   write(6,*)
@@ -212,6 +241,151 @@ subroutine SO3_rotation(nx,ny,nz,theta, SO3_matrix)
   SO3_matrix(3,2) = (1.0d0 - cos(theta))*nz*ny + nx*sin(theta)
   SO3_matrix(3,3) = (1.0d0 - cos(theta))*nz*nz + cos(theta)
    
+  
+  return
+end subroutine
+
+
+subroutine from_SO3_matrix_to_SU2_matrix(SO3_matrix,SU2_matrix)
+  implicit none
+                        
+  real(8),intent(in):: SO3_matrix(3,3)
+  complex(8),intent(out)::SU2_matrix(2,2)
+  
+  real(8):: s(3,3),det
+  
+  real(8)::nx,ny,nz
+  real(8)::theta,sintheta,costheta
+  real(8)::tol=1e-3,diff
+  
+  real(8)::matrix1(3,3),matrix2(3,3), Imatrix(3,3)
+  integer::ii,jj,kk,nn
+  real(8),parameter::pi=dacos(-1.0d0)
+  logical::isInv=.false.
+      
+  
+  s=SO3_matrix
+  det = s(1,1) * ( s(2,2) * s(3,3) - s(3,2) * s(2,3) )-   &
+        s(1,2) * ( s(2,1) * s(3,3) - s(3,1) * s(2,3) )+   &
+        s(1,3) * ( s(2,1) * s(3,2) - s(3,1) * s(2,2) )
+  
+  if(abs(det+1.d0) < tol) then
+    s=-SO3_matrix
+    isInv=.true.
+  endif
+  
+  
+  Imatrix(:,:) = 0.0d0 
+  do ii=1,3
+    Imatrix(ii,ii) = 1.0d0
+  enddo
+  
+  matrix1 = s
+  matrix2 = s
+  
+  do nn=1,10
+    
+    diff=0.0d0
+    do ii=1,3
+      do jj=1,3
+        diff = diff + abs(matrix2(ii,jj) - Imatrix(ii,jj))
+      enddo
+    enddo
+    
+    if(diff < tol) exit
+    matrix2(:,:) = 0.0d0 
+    
+    do ii=1,3
+      do jj=1,3
+        do kk=1,3
+          matrix2(ii,jj) = matrix2(ii,jj) + matrix1(ii,kk)*s(kk,jj)
+        enddo
+      enddo
+    enddo
+    
+    !write(6,*) 'nn=', nn, diff
+    !write(6,*)
+    !write(6,fmt='(3(1x,F10.5,"        ",1x))') ((SO3_matrix(ii,jj), jj = 1,3), ii = 1,3)
+    !write(6,*)
+    !write(6,fmt='(3(1x,F10.5,"        ",1x))') ((matrix2(ii,jj), jj = 1,3), ii = 1,3)
+    !write(6,*) 
+    !write(6,fmt='(3(1x,F10.5,"        ",1x))') ((matrix1(ii,jj), jj = 1,3), ii = 1,3)
+    !write(6,*) 
+    
+    matrix1(:,:)=matrix2(:,:)
+  enddo
+  
+  theta = pi*2.0d0/(nn)
+  
+  !write(6,*)
+  !write(6,*) 'N=', nn, theta
+  !SO3_matrix(1,1)-SO3_matrix(1,1)
+  
+  sintheta = sin(theta)
+  costheta = cos(theta)
+  
+  if(nn==1) then
+    nx = 0
+    ny = 0 
+    nz = 0  !does not matter
+    theta=0.0d0 !matter
+
+  elseif(nn==2) then !more complicated
+    matrix2(:,:) = s(:,:)
+    do ii=1,3
+      do jj=1,3
+        matrix2(ii,jj) = 0.5d0*(matrix2(ii,jj) + Imatrix(ii,jj))
+      enddo
+    enddo
+    if(matrix2(1,1)>tol) then
+      nx = sqrt(matrix2(1,1)) ! choose positive (can for 180 angle)
+      ny = matrix2(1,2) / nx
+      nz = matrix2(1,3) / nx
+    elseif(matrix2(2,2)>tol) then
+      ny = sqrt(matrix2(2,2)) ! same
+      nx = matrix2(2,1) / ny
+      nz = matrix2(2,3) / ny    
+    elseif(matrix2(3,3)>tol) then
+      nz = sqrt(matrix2(3,3)) ! same
+      nx = matrix2(3,1) / nz
+      ny = matrix2(3,2) / nz        
+    else
+      write(6,*) 'error with SO3 matrix'
+      write(6,fmt='(3(1x,F10.5,"        ",1x))') ((s(ii,jj), jj = 1,3), ii = 1,3)
+      write(6,*) 'error with SO3 matrix'
+      write(6,fmt='(3(1x,F10.5,"        ",1x))') ((matrix2(ii,jj), jj = 1,3), ii = 1,3)
+      stop
+    endif
+    
+!    if (abs(ny)<tol) then
+!      if(abs(nx+nz)<tol) then
+!        nx = -nx
+!        nz = -nz
+!      endif
+!    endif
+        
+  else !nn>2
+  
+    nx = 0.5d0*(s(3,2) - s(2,3)) / sintheta
+    ny = 0.5d0*(s(1,3) - s(3,1)) / sintheta
+    nz = 0.5d0*(s(2,1) - s(1,2)) / sintheta
+    
+  endif
+  
+  !write(6,*) 'n=', nx,ny,nz
+  
+  if(isInv) then
+    call SU2_rotation(nx,ny,nz,-theta/2.0d0,SU2_matrix)
+  else
+    call SU2_rotation(nx,ny,nz,theta/2.0d0,SU2_matrix)
+  endif
+  
+  !if (costheta < -tol ) SU2_matrix=-SU2_matrix
+  !if (abs(costheta) < tol ) then
+  !  if(nx*nz < -tol) then
+  !    SU2_matrix = -SU2_matrix
+  !  endif
+  !endif
   
   return
 end subroutine
