04c7a67bbeb6439eef54c73e87e14bbb47071fd6
kazuma <kazuma@mns.kyutech.ac.jp>
2019-01-10 17:02:24 +0900

    CMAKE information updated

--- config/intel.cmake
+++ config/intel.cmake
@@ -1,6 +1,8 @@
 # for Intel Compiler
 # to detect omp flag
 set(CMAKE_C_COMPILER "icc" CACHE STRING "" FORCE)
+set(CMAKE_MPI_Fortran_COMPILER "mpiifort" CACHE STRING "" FORCE)
+set(CMAKE_MPI_Fortran_FLAGS_RELEASE " -O2 -qopenmp -xHost -mcmodel=large -shared-intel")  
 set(CMAKE_Fortran_COMPILER "ifort" CACHE STRING "" FORCE)
 set(CMAKE_Fortran_FLAGS_RELEASE " -O2 -g -fbacktrace -DHAVE_SSE2 -DNDEBUG -mcmodel=large -shared-intel")
 
