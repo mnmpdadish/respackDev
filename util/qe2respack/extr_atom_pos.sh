ed531cc4e182fa932e86d0bb450e9005d81038c9
Maxime Charlebois <mnmpdadish@hotmail.com>
2019-01-28 01:54:15 -0500

    minor changes

--- util/qe2respack/extr_atom_pos.sh
+++ util/qe2respack/extr_atom_pos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-natom=`grep 'nat' ./*.scf.in | awk -F'[=,]' '{print $2}'`
+natom=`grep 'nat' ./*.scf.in | awk -F'[=,\r]' '{print $2}'`
 echo $natom > ./dat.atom_position
 grep -A $natom 'ATOMIC_POSITIONS' $1 | tail -n$natom >> ./dat.atom_position
 mv dat.atom_position ./dir-wfn/.
