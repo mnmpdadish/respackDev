241b8c40a0e9799fe5ab14e3b8fdcb85c61a4518
kazuma <kazuma@mns.kyutech.ac.jp>
2019-05-05 17:10:19 +0900

    +++ Unnecessary file 'extr_atom_position.sh' deleted +++

--- util/extr_atom_position.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-natom=`grep 'nat' ./*.scf.in | awk '{print $3}'` 
-echo $natom > ./dat.atom_position 
-grep -A $natom 'ATOMIC_POSITIONS' ./*.scf.in | tail -$natom >> ./dat.atom_position 
-
